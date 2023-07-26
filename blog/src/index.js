29f9fc919a07b5b32ce92ee3208263b40900de6f
Nguyen Van Ty <anhhung_so11@yahoo.com>
2023-07-26 09:26:27 +0700

    update

---
+++ blog/src/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const handlebars = require("express-handlebars");
+const port = 3000;
+const morgan = require("morgan");
+// http logger
+app.use(morgan("combined"));
+// template engine
+// hbs = handlebars
+app.engine(
+  "hbs",
+  handlebars.engine({
+    extname: ".hbs",
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", "./src/resources/views");
+console.log("-----");
+console.log(__dirname);
+app.get(
+  "/",
+  (req, res) => res.render("home")
+  //   res.send(`
+  //   <h1 style="color: red" >Trang chủ</h1>
+  // `)
+);
+app.get("/news", (req, res) => res.render("news"));
+// 127.0.0.1 - localhost
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+);
